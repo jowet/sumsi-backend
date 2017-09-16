041ceba7179f7a60ca7f078f1f82201d8f9f4509
Roland Koch <rojoko@gmail.com>
2017-09-16 21:09:38 +0200

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,11 @@ ledger = Ledger.create(balance: 0)
 
 # TASKS
 2.times do
-  Task.create(title: 'Clean room',       value: 100, ledger: ledger, state: 'completed')
-  Task.create(title: 'Fill dishwasher',  value: 100, ledger: ledger, state: 'completed')
-  Task.create(title: 'Empty dishwasher', value: 300, ledger: ledger, state: 'completed')
+  Task.create(title: 'Clean your room',      value: 100, ledger: ledger, state: 'completed')
+  Task.create(title: 'Fill the dishwasher',  value: 100, ledger: ledger, state: 'completed')
+  Task.create(title: 'Empty the dishwasher', value: 300, ledger: ledger, state: 'completed')
 end
-Task.create(title: 'Bring out trash', value: 37, ledger: ledger, state: 'completed')
+Task.create(title: 'Bring out the trash', value: 37, ledger: ledger, state: 'completed')
 Task.all.map(&:close!)
 
-Task.create(title: 'Empty dishwasher', value: 300, ledger: ledger)
+Task.create(title: 'Empty the dishwasher', value: 300, ledger: ledger)
