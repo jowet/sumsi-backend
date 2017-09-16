fc1cc034f629a00694de8b63f9248d55a4b92a0f
Roland Koch <rojoko@gmail.com>
2017-09-16 23:51:17 +0200

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,12 +24,11 @@ AdminUser.create!(email: 'sumsi@example.com', password: '123123', password_confi
 ledger = Ledger.create(balance: 0)
 
 # TASKS
-2.times do
-  Task.create(title: 'Clean your room',      value: 100, ledger: ledger, state: 'completed')
-  Task.create(title: 'Fill the dishwasher',  value: 100, ledger: ledger, state: 'completed')
-  Task.create(title: 'Empty the dishwasher', value: 300, ledger: ledger, state: 'completed')
-end
-Task.create(title: 'Bring out the trash', value: 37, ledger: ledger, state: 'completed')
+Task.create(title: 'Clean your room', value: 150, ledger: ledger, state: 'completed')
+Task.create(title: 'Walk the dog',    value: 350, ledger: ledger, state: 'completed')
+Task.create(title: 'Bring out trash', value: 100, ledger: ledger, state: 'completed')
+Task.create(title: 'Finish homework', value: 250, ledger: ledger, state: 'completed')
+Task.create(title: 'Finish homework', value: 187, ledger: ledger, state: 'completed')
 Task.all.map(&:close!)
 
-Task.create(title: 'Empty the dishwasher', value: 300, ledger: ledger)
+Task.create(title: 'Clean your room', value: 300, ledger: ledger)
